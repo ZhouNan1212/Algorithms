05b72376dce743fbe9d2deffdb6678e2eb2351da
周楠 <906037502@qq.com>
2017-12-19 22:13:02 +0800

    二叉树的delete方法

--- src/main/java/HashTable.java
+++ src/main/java/HashTable.java
@@ -229,6 +229,35 @@ public class HashTable {
             else if (cmp > 0) return 1 + size(x.left) + rank(key, x.right);
             else              return size(x.left);
         }
+
+        private void deleteMin() {root = deleteMin(root);}
+
+        private Node deleteMin(Node x) {
+            if (x == null) return null;
+            x.left = deleteMin(x.left);
+            x.N = size(x.left) + size(x.right) + 1;
+            return x;
+        }
+
+        private void delete(Key key) {root = delete(root, key);}
+
+        private Node delete(Node x, Key key) {
+            if (x == null) return null;
+            int cmp = key.compareTo(x.key);
+            if (cmp < 0) x.left = delete(x.left, key);
+            else if (cmp > 0) x.right = delete(x.right, key);
+            else {
+                if (x.right == null) return x.left;
+                if (x.left == null) return x.right;
+                Node t = x;
+                x = min(t.right);
+                x.right = deleteMin(t.right);
+                x.left = t.left;
+            }
+            x.N = size(x.left) + size(x.right) + 1;
+            return x;
+        }
+
     }
 
 
